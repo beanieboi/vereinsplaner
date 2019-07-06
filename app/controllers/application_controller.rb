5723fde6ade7827b248e005b72dd574bf5e5cfdc
ben <beanie@benle.de>
2019-07-06 13:34:27 +0200

    German locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
 
   before_action :login_required
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = :de
+  end
 end
