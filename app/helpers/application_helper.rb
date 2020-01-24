58377c461e01c03db79e5199e27e192a354c9d6d
ben <beanie@benle.de>
2020-01-24 23:20:16 +0100

    Use more icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
   def last_payment_for_table(member)
-    return "Lastschrifteinzug" if member.sepa_mandate?
+    return fa_icon("coins", style: 'color: #00ad00') if member.sepa_mandate?
 
     last_payment = member.last_payment
     if last_payment
-      last_payment.payed_at
+      if last_payment.payed_at > 10.months.ago
+        fa_icon("coins", style: 'color: #00ad00')
+      else
+        fa_icon("coins", style: 'color: #e52c00')
+      end
     else
-      "keine Zahlung vorhanden"
+      fa_icon("coins", style: 'color: #e52c00')
     end
   end
 
