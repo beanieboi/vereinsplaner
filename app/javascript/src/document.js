90735c0aed90987db6380227223496cc84708629
Kristin Fritsch <ich@kristin-fritsch.de>
2020-01-25 13:46:11 +0100

    Fix nav collapsing

--- app/javascript/src/document.js
+++ app/javascript/src/document.js
@@ -12,14 +12,19 @@
         }
       });
     }
-  });
 
-  var navBars = document.getElementsByClassName('navbar-toggler');
-    navBars.forEach(function(navBar) {
-    navBar.addEventListener('click', function() {
-      var target = ele.dataset.target;
-      var navItems = document.getElementById(target);
-      navItems.classList.toggle('collapse')
-    })
+    var navBars = document.getElementsByClassName('navbar-toggler');
+    if (navBars.length > 0) {
+      navBars[0].addEventListener('click', function(event) {
+        var target = event.target.dataset.target;
+        if (!target) {
+          target = event.target.parentElement.dataset.target;
+        }
+        if (target) {
+          var navItems = document.getElementById(target);
+          navItems.classList.toggle('collapse')
+        }
+      });
+    }
   });
 })();
