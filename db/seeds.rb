bcea2c5c63c5f4e5a4e167c93be47fcc3322cfac
ben <beanie@benle.de>
2020-01-25 13:22:00 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+MembershipType.create(
+  short: "full",
+  name: "Aktive Mitgliedschaft"
+)
+
+MembershipType.create(
+  short: "discounted",
+  name: "ermäßigte Mitgliedschaft"
+)
+
+MembershipType.create(
+  short: "passive",
+  name: "Passive Mitgliedschaft"
+)
+
+MembershipType.create(
+  short: "support",
+  name: "Fördermitgliedschaft"
+)
+
+MembershipType.create(
+  short: "unknown",
+  name: "Unbekannt"
+)
